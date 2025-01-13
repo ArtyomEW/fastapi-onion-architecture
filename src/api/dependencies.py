24309dfed957480dc7d173fcf8bd7b158b92ab29
ArtyomEW <152199050+ArtyomEW@users.noreply.github.com>
2025-01-13 16:44:07 +0300

    Update dependencies.py

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,7 +1,5 @@
-from typing import Annotated
-
-from fastapi import Depends
-
 from utils.unitofwork import IUnitOfWork, UnitOfWork
+from typing import Annotated
+from fastapi import Depends
 
 UOWDep = Annotated[IUnitOfWork, Depends(UnitOfWork)]
